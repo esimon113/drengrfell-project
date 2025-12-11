240e9668e63f78b6414b611c248c1e517f0db36f
Tim Ernst <tim.ernst@uni-bielefeld.de>
2025-12-11 22:36:32 +0100

    Improve texture array

--- src/utils/textureArray.cpp
+++ src/utils/textureArray.cpp
@@ -30,34 +30,41 @@ namespace df {
     /**
      * Assumes a vertical stripe of quadratical tile textures
      */
-    TextureArray TextureArray::init(const assets::Texture asset, const int tile_height) noexcept {
+    TextureArray TextureArray::init(const assets::Texture asset, const int widthOfSprite, int heightOfSprite) noexcept {
         const std::string assetPath = assets::getAssetPath(asset);
-        return init(assetPath.c_str(), tile_height);
+        return init(assetPath.c_str(), widthOfSprite, heightOfSprite);
     }
 
     /**
      * Assumes a vertical stripe of rectangular tile textures
      */
-    TextureArray TextureArray::init(const char* path, const int tile_height) noexcept {
+    TextureArray TextureArray::init(const char* path, int widthOfSprite, int heightOfSprite) noexcept {
         int width, height, channels;
         stbi_set_flip_vertically_on_load(true);
         stbi_uc* pixels = stbi_load(path, &width, &height, &channels, 4);
 
-        if (height % tile_height != 0) {
-            std::cerr << "TextureArray::init: height is not divisible by tile_height" << std::endl;
+        if (width % widthOfSprite != 0) {
+            std::cerr << "TextureArray::init: width is not divisible by widthOfSprite" << std::endl;
         }
-        int tiles = height / tile_height;
-        int tile_width = width;
+        if (height % heightOfSprite != 0) {
+            std::cerr << "TextureArray::init: height is not divisible by heightOfSprite" << std::endl;
+        }
 
         TextureArray self;
+        self.spriteWidth = widthOfSprite;
+        self.spriteHeight = heightOfSprite;
+        self.spritesInX = width / widthOfSprite;
+        self.spritesInY = height / heightOfSprite;
+
         glGenTextures(1, &self.handle);
         glBindTexture(GL_TEXTURE_2D_ARRAY, self.handle);
+
         glTexImage3D(GL_TEXTURE_2D_ARRAY,
              0,                 // mipmap level
              GL_RGBA8,          // gpu texel format
-             tile_width,        // width
-             tile_height,       // height
-             tiles,             // depth
+             widthOfSprite,     // width
+             heightOfSprite,    // height
+             static_cast<int>(self.getSpriteCount()),             // depth
              0,                 // border
              GL_RGBA,           // cpu pixel format
              GL_UNSIGNED_BYTE,  // cpu pixel coord type
--- src/utils/textureArray.h
+++ src/utils/textureArray.h
@@ -8,20 +8,38 @@
 #include <common.h>
 #include <assets.h>
 
+#include "glm/gtx/io.hpp"
+
+/*
+ * Sprite: A subimage in a sprite sheet
+ */
+
 namespace df {
     class TextureArray {
     public:
         static TextureArray init(GLsizei width, GLsizei height) noexcept;
-        static TextureArray init(assets::Texture asset, int tile_height = 64) noexcept;
-        static TextureArray init(const char* path, int tile_height = 64) noexcept;
+        static TextureArray init(assets::Texture asset, int widthOfSprite = 64, int heightOfSprite = 64) noexcept;
+        static TextureArray init(const char* path, int widthOfSprite = 64, int heightOfSprite = 64) noexcept;
 
         void deinit() noexcept;
         void bind(GLuint sampler) const noexcept;
         operator int() const noexcept { return handle; }
 
+        [[nodiscard]] std::size_t getSpriteWidth() const noexcept { return spriteWidth; }
+        [[nodiscard]] std::size_t getSpriteHeight() const noexcept { return spriteHeight; }
+        [[nodiscard]] std::size_t getSpritesInX() const noexcept { return spritesInX; }
+        [[nodiscard]] std::size_t getSpritesInY() const noexcept { return spritesInY; }
+        [[nodiscard]] std::size_t getSpriteCount() const noexcept {
+            return spritesInX * spritesInY;
+        }
 
     private:
         GLuint handle;
+
+        std::size_t spriteWidth;
+        std::size_t spriteHeight;
+        std::size_t spritesInX;
+        std::size_t spritesInY;
     };
 }
 
