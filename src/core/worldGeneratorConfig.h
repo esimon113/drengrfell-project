90b648d2ea51219b09f5decb56370c3c9b07e127
Tim Ernst <tim.ernst@uni-bielefeld.de>
2025-12-15 00:16:41 +0100

    Add comments to world generator config

--- src/core/worldGeneratorConfig.h
+++ src/core/worldGeneratorConfig.h
@@ -6,15 +6,19 @@ namespace df {
             INSULAR, // Like in the classic board game
             PERLIN,  // TODO: Find better name
         };
-        GenerationMode generationMode = GenerationMode::INSULAR;
+        GenerationMode generationMode = GenerationMode::PERLIN;
 
         // General
-        unsigned seed = 123;
-        unsigned columns = 24;
-        unsigned rows = 24;
+        unsigned seed = 123; // The same seed creates the same world
+        unsigned columns = 24; // Size in x direction
+        unsigned rows = 24; // Size in y direction
 
         // Noise
-        float persistence = 0.5f;
-        unsigned octaves = 1;
+        struct AltitudeNoiseConfig {
+            float frequency = 0.1f; // Same meaning as in sine waves
+            float persistence = 0.50f; // The higher, the more similar to swiss cheese
+            unsigned octaves = 12; // The "granularity" of the map. Look up Fractal Brownian Motion
+        } altitudeNoise;
+
     };
 }
