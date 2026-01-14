4d5f50334113de1bb30c6510bfe873e05b943ff7
Fabian Daniel Schütte <fschuette@techfak.uni-bielefeld.de>
2026-01-14 21:51:08 +0100

    Correctly center the camera onto the hero at the game start

--- src/application.cpp
+++ src/application.cpp
@@ -180,6 +180,9 @@ namespace df {
 			if (gamePhase == types::GamePhase::PLAY && previousGamePhase != types::GamePhase::PLAY) {
 				gameController->startTurn(*registry);
 				fmt::println("Turn started for player {}", gameState->getCurrentPlayerId());
+				// Prepare the camera so it can be centered
+				world.step(0.0f);
+				world.centerCameraOnPoint(movementSystem.getTargetPosition());
 			}
 
 			switch (gamePhase) {
@@ -472,7 +475,6 @@ namespace df {
 			registry->tileID.emplace(hero, randomTileID);
 		}
 		movementSystem.setTarget(randomTileID, hero);
-		world.centerCameraOnPoint(registry->positions.get(hero));
 	}
 
 	void Application::onMouseButtonCallback(GLFWwindow* windowParam, int button, int action, int mods) noexcept {
